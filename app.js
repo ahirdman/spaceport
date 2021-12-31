f9848eaef6c5f606a478f116815c7a715aa65aa4
Alexander Hirdman <hirdman.alex@icloud.com>
2021-12-31 17:57:34 +0100

    Refactor and ready for deployment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 8000; // Deply on roku
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -6,38 +7,39 @@ const app = express();
 
 const launches = [];
 
-app.get('/', (req, res) => {
-  axios
-    .get('https://spaceflightnow.com/launch-schedule/')
-    .then(response => {
-      const html = response.data;
-      const $ = cheerio.load(html);
+axios.get('https://spaceflightnow.com/launch-schedule/').then(response => {
+  const html = response.data;
+  const $ = cheerio.load(html);
 
-      $('div.datename', html).each(function () {
-        const date = $(this).children('.launchdate').text();
-        const headerArr = $(this).children('.mission').text().split(' • ');
-        const siteArr = $(this).next().text().split('Launch site: ');
-        const infoArr = $(this).next().next().text().split(' [');
+  $('div.datename', html).each(function () {
+    const date = $(this).children('.launchdate').text();
+    const headerArr = $(this).children('.mission').text().split(' • ');
+    const siteArr = $(this).next().text().split('Launch site: ');
+    const infoArr = $(this).next().next().text().split(' [');
 
-        const mission = headerArr[1];
-        const rocket = headerArr[0];
-        const site = siteArr[1];
-        const info = infoArr[0];
+    const mission = headerArr[1];
+    const rocket = headerArr[0];
+    const site = siteArr[1];
+    const info = infoArr[0];
 
-        launches.push({
-          date,
-          mission,
-          rocket,
-          site,
-          info,
-        });
-      });
+    launches.push({
+      date,
+      mission,
+      rocket,
+      site,
+      info,
+    });
+  });
 
-      res.json(launches);
-    })
-    .catch(err => console.error(err));
+  app.get('/', (req, res) => {
+    res.json('Startpage');
+  });
+
+  app.get('/launches', (req, res) => {
+    res.json(launches);
+  });
 });
 
-app.listen(3000, () => {
-  console.log('App listening on "localhost:3000"');
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
 });
