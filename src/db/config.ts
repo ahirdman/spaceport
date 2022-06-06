3b82eb29634eaf9d8551fb98cd3a008cfb05d3a6
Alexander Hirdman <hirdman.alex@icloud.com>
2022-06-06 09:08:41 +0200

    Test 2

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,3 +1,7 @@
-setInterval(() => {
-  console.log('Hello World');
-}, 5000);
+const scriptRunner = () => {
+  setInterval(() => {
+    console.log('Hello World');
+  }, 5000);
+};
+
+scriptRunner();
