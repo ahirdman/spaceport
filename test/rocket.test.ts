572e17b31e999835a29fafb04629aa7971bcde67
Alexander Hirdman <hirdman.alex@icloud.com>
2022-06-23 10:36:30 +0200

    Workflow

--- test/rocket.test.ts
+++ test/rocket.test.ts
@@ -1,14 +1,15 @@
 import app from '../src/api/app';
 import request from 'supertest';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import { insertData } from './../src/data/index';
 
 describe('API - "Rockets"', () => {
-  if (process.env.NODE_ENV === 'ci') {
-    console.log('howdy console');
-  }
-
   describe('all', () => {
+    it('notes the node_env', () => {
+      const env = process.env.NODE_ENV === 'ci';
+      assert.equal(env, true);
+    });
+
     it('responds with json', done => {
       request(app).get('/api/rockets').expect('Content-Type', /json/).expect(200, done);
     });
